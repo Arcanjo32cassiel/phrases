5a907ae0b4627cae32a5901508535fc00b690c5b
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2020-11-16 08:44:43 -0300

    data processing

--- phrases.js
+++ phrases.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 const data = require('./data.json')
-    // create
-exports.post = function(req, res) {
-    // req.body
-    // {frase:	"fdfd",autor:"dfdf", foto:"http://localhost:3000/phrases/addphrase"}
 
-    //[	"frase",	"autor","foto"]
+// create
+exports.post = function(req, res) {
     const keys = Object.keys(req.body)
 
+    // validate
     for (key of keys) {
         if (req.body[key] == '') {
             return res.send('please, fill all field')
         }
     }
+    //  data processing
+    let { phrase, author, photograph } = req.body
 
-    // []
-    data.phrases.push(req.body) //[{...}]
-        // data.phrases.push(req.body) //[{...}]
+    req.body.id = Number(data.phrases.length + 1)
+        // deconstruction
+    data.phrases.push({
+        phrase,
+        author,
+        photograph
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file erro!")
