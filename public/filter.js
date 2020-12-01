c93669585e935c0eb5e162973984848218dcaf17
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2020-12-01 08:40:09 -0300

    new styles in layout and in input filter

---
+++ public/filter.js
@@ -0,0 +1,13 @@
+// const dataphrases = new Request('data.json')
+
+const dataparse = JSON.parse('data.json')
+
+console.log(dataparse[0])
+
+
+// const getposts = async() => {
+//     const response = await fetch(dataparse)
+
+//     console.log(response)
+// }
+// getposts()
